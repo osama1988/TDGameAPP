86d6a169e12295ec27319d313737ee1dc29b9b62
Mayur Patil <mrpatil@outlook.com>
2014-11-15 16:54:26 -0500

    Factory Pattern

---
+++ src/com/tdgame/TowerFactory.java
@@ -0,0 +1,25 @@
+package com.tdgame;
+
+public class TowerFactory {
+
+	static public Tower getTower(String towerType)
+	{
+		
+		if (towerType.equalsIgnoreCase("FIRE")){
+			return new Fire();
+		}
+        else if (towerType.equalsIgnoreCase("LASER")){
+        	return new Laser();
+        }
+        else if (towerType.equalsIgnoreCase("BOMBER")){
+        	return new Bomber();
+        }
+        else if (towerType.equalsIgnoreCase("TANK")){
+        	return new Tank();
+        }
+        else if (towerType.equalsIgnoreCase("MISSILE")){
+        	return new Missile();
+        }
+		return null;
+	}
+}
