b390c429c269a7a5b773a4545576d533a18ff301
Manpreet Singh <manpreet.singh@live.ca>
2014-11-16 21:12:17 -0500

    ADD: Created test cases for Tower class and updated the testSuite

--- src/com/tdgame/TestSuite.java
+++ src/com/tdgame/TestSuite.java
@@ -9,11 +9,13 @@ import org.junit.runner.RunWith;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	CritterTest.class,
+	BomberTest.class,
 	ScreenTest.class,
 	ReadXMLTest.class,
-//	TowerTest.class,
+	TowerTest.class,
 	ActionHandlerTest.class
 	})
+
 public class TestSuite {
-  //nothing
+  
 }
--- src/com/tdgame/TowerTest.java
+++ src/com/tdgame/TowerTest.java
@@ -2,46 +2,75 @@ package com.tdgame;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TowerTest {
-
-	@Test
-	/**
-	 * Testing if the image file is available
-	 * Checking if the object is not null and are not pointing to the same memory address.
-	 */
-	public void testTowerObjects() {
-		/*Tower tower1 = new Tower(1, "");
-		Tower tower2 = new Tower(1, "");
-		assertNotSame(tower1, tower2);
-		assertNotNull(tower2);*/
-	}
 	
-	/**
-	 * Testing if we have all the specified towers in the array
-	 */
+	Bomber bomber;
 	
-	@Test
-	public void testTowerListArray(){
-		/*Tower tower = new Tower();
-		String expectedTowerNames[]={"Laser","Tank","Fire","Missile","Bomber"};
-		assertArrayEquals(expectedTowerNames, tower.towerNames);*/
+	@Before
+	public void beforeContext() { 
+	    bomber = new Bomber();
 		
 	}
 	
 	/**
-	 * Checking if the towers list array is not null
+	 * Testing health before attack is made and after attack is made
 	 */
 	@Test
-	public void test() {
-		/*Tower lighteningTower = new TowerTypes(id, cost, range,"");
-		if(lighteningTower.towerList[id] != null)
-		{
-			towerIdRepeatCheck = true;
-		}
-		assertTrue(towerIdRepeatCheck);*/
+	public void comparingHealth() {
+		Critter critter = new Critter();
+		int orig_health = critter.health;
+		bomber.towerAttack(5, 5, critter);
+		assertNotSame(critter.health, orig_health);
+		
 	}
 	
-
+	/**
+	 * Testing health before attack is made and after attack is made
+	 */
+	@Test
+	public void setDamageToCritters() {
+		bomber.setDamageToCritters(20);
+		assertEquals(20, bomber.getDamageToCritters());
+		
+	}
+	
+	/**
+	 * Testing if the attack time is being set properly by getting it from get method
+	 */
+	@Test
+	public void setAttackTime() {
+		bomber.setAttackTime(10);
+		assertEquals(10, bomber.getAttackTime());
+	}
+	
+	
+	/**
+	 * Testing if the attack delay is being set properly by getting it from get method
+	 */
+	@Test
+	public void setAttackDelay() {
+		bomber.setAttackDelay(10);
+		assertEquals(10, bomber.getAttackDelay());
+	}
+	
+	/**
+	 * Testing if the attack Max Attack Time is being set properly by getting it from get method
+	 */
+	@Test
+	public void setMaxAttackTime() {
+		bomber.setMaxAttackTime(10);
+		assertEquals(10, bomber.getMaxAttackTime());
+	}
+	
+	/**
+	 * Testing if the attack Max Attack Delay is being set properly by getting it from get method
+	 */
+	@Test
+	public void setMaxAttackDelay() {
+		bomber.setMaxAttackDelay(10);
+		assertEquals(10, bomber.getMaxAttackDelay());
+	}
 }
